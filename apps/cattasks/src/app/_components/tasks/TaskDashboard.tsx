097a166ea3725a234d5141d2da9955525c9c5314
Olena Catbug <kraizkot@gmail.com>
2025-05-25 00:38:07 +0500

    Инициализирован новый проект cattasks на Next.js. Добавлена базовая структура с основными компонентами и стилями: TaskDashboard с заголовком и описанием, TDHeader с выпадающим списком задач (TDHeaderTaskList), реализованным через useState для управления раскрытием. Созданы пустые компоненты и стили для основной логики приложения (TDMain, TDMainCodeEditor, TDMainTaskDetails, TDMainTests) для дальнейшей разработки. В проекте предусмотрена модульная организация CSS с отдельными файлами стилей для каждого компонента.

---
+++ apps/cattasks/src/app/_components/tasks/TaskDashboard.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from 'react';
+import TDHeader from '@/app/_components/tasks/TDHeader';
+
+export default function TaskDashboard() {
+  return (
+      <div>
+    <TDHeader>
+      <h1>Task Dashboard</h1>
+      <p>This is the Task Dashboard component.</p>
+    </TDHeader>
+        </div>
+  );
+}
---
+++ apps/cattasks/src/app/page.tsx
@@ -0,0 +1,8 @@
+import TaskDashboard  from "@/app/_components/tasks/TaskDashboard";
+
+
+export default function Home() {
+  return (
+      <TaskDashboard />
+  );
+}
