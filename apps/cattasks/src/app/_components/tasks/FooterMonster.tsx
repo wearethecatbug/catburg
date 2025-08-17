da9337da71caf9c933467afe572ca722c3dc50a1
Olena Catbug <kraizkot@gmail.com>
2025-08-17 16:51:50 +0500

    New component FooterMonster was created. It has css sprite. Soon'll be animated.

---
+++ apps/cattasks/src/app/_components/tasks/FooterMonster.tsx
@@ -0,0 +1,8 @@
+import styles from './FooterMonster.module.css';
+
+export default function FooterMonster() {
+    return (
+        <div className={styles.monsterSprite}>
+        </div>
+    );
+}
