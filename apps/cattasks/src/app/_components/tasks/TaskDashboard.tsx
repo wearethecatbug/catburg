04e8ee16d638f37049328bc57c9a08d52cefe87c
Olena Catbug <kraizkot@gmail.com>
2025-08-17 16:27:00 +0500

    components Main and Footer were added to a dom-tree +headerContainer, mainContainer, footerContainer css

--- apps/cattasks/src/app/_components/tasks/TaskDashboard.tsx
+++ apps/cattasks/src/app/_components/tasks/TaskDashboard.tsx
@@ -1,12 +1,14 @@
-import Header from '@/app/_components/tasks/Header';
 import styles from './TaskDashboard.module.css';
+import Header from '@/app/_components/tasks/Header';
+import Main from '@/app/_components/tasks/Main';
+import Footer from '@/app/_components/tasks/Footer';
 
 export default function TaskDashboard() {
     return (
         <div className={styles.tdContainer}>
             <Header className={styles.headerContainer}></Header>
-            {/*<Main></Main>*/}
-            {/*<Footer></Footer>*/}
+            <Main className={styles.mainContainer}></Main>
+            <Footer className={styles.footerContainer}></Footer>
         </div>
     );
 }
