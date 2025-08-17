489fb2f6728232dd9c6faef9e6038b6081c052e6
Olena Catbug <kraizkot@gmail.com>
2025-08-17 16:28:01 +0500

    components were deleted

--- apps/cattasks/src/app/_components/tasks/HeaderSearchInput.tsx
+++ apps/cattasks/src/app/_components/tasks/HeaderSearchInput.tsx
@@ -1,16 +1,21 @@
-import styles from './HeaderSearchInput.module.css';
-
-export default function HeaderSearchInput() {
-    return (
-        <div className={styles.searchInput}>
-            <input
-                className={styles.inputField}
-                type="text"
-                placeholder="Поиск..."
-                autoComplete="off"
-                autoCorrect="off"
-                spellCheck="false"
-            />
-        </div>
-    );
-}
+// import styles from './HeaderSearchInput.module.css';
+//
+// interface HeaderSearchInputProps {
+//     readonly className?: string;
+//     readonly children?: React.ReactNode;
+// }
+//
+// export default function HeaderSearchInput({className, children}: HeaderSearchInputProps) {
+//     return (
+//         <div className={styles.searchInput}>
+//             <input
+//                 className={styles.inputField}
+//                 type="text"
+//                 placeholder="Поиск..."
+//                 autoComplete="off"
+//                 autoCorrect="off"
+//                 spellCheck="false"
+//             />
+//         </div>
+//     );
+// }
