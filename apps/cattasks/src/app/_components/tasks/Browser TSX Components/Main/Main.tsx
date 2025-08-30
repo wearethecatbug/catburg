a9fead842da4d6c3ddc74e3864900dad196ee6dc
Olena Catbug <kraizkot@gmail.com>
2025-08-31 01:10:07 +0500

    Rename Main module files for improved organization and update import paths

--- apps/cattasks/src/app/_components/tasks/Browser TSX Components/Main/Main.tsx
+++ apps/cattasks/src/app/_components/tasks/Browser TSX Components/Main/Main.tsx
@@ -1,13 +1,13 @@
 import styles from './Main.module.css';
-import MainTaskDetails from "@/app/_components/tasks/MainTaskDetails";
-import MainCodeEditor from "@/app/_components/tasks/MainCodeEditor";
+import MainTaskDetails from "./MainTaskDetails";
+import MainCodeEditor from "./MainCodeEditor";
 
 interface MainProps {
     readonly className?: string;
-    readonly children?: React.ReactNode;
+    // readonly children?: React.ReactNode;
 }
 
-export default function Main({className, children}: MainProps) {
+export default function Main({className}: MainProps) {
     return (
         <div className={styles.mainContainer}>
             <MainTaskDetails className={styles.taskDetails}/>
