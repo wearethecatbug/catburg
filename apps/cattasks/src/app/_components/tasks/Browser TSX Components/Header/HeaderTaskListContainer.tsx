b772679a2716c75b001ec870b5658dbbf82aa3b6
Olena Catbug <kraizkot@gmail.com>
2025-08-31 01:15:34 +0500

    Rename HeaderTaskListContainer files for improved organization and update import paths

--- apps/cattasks/src/app/_components/tasks/Browser TSX Components/Header/HeaderTaskListContainer.tsx
+++ apps/cattasks/src/app/_components/tasks/Browser TSX Components/Header/HeaderTaskListContainer.tsx
@@ -1,15 +1,15 @@
-import HeaderTaskListButton from "@/app/_components/tasks/HeaderTaskListButton";
-import HeaderArrowButtons from "@/app/_components/tasks/HeaderArrowButtons";
+import HeaderTaskListButton from "./HeaderTaskListButton";
+import HeaderArrowButtons from "./HeaderArrowButtons";
 import styles from './HeaderTaskListContainer.module.css';
 import React from "react";
 
 
 interface HeaderTaskListContainerProps {
-    readonly children?: React.ReactNode;
+    // readonly children?: React.ReactNode;
     readonly className?: string;
 }
 
-export default function HeaderTaskListContainer({children, className}: HeaderTaskListContainerProps) {
+export default function HeaderTaskListContainer({className}: HeaderTaskListContainerProps) {
     return (
         <div className={styles.taskListContainer}>
             {/*<button className={styles.arrowLeft}/>*/}
